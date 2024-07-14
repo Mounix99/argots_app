282e651b6f65f52b50e003b21c93e4e642fa692f
Nikita Mikhaulov <Sience1999@gmail.com>
2024-07-14 18:43:08 +0200

    plant_edit_cubit_and_some_ui

--- lib/features/plant/edit_plant/edit_plant_cubit.dart
+++ lib/features/plant/edit_plant/edit_plant_cubit.dart
@@ -3,14 +3,26 @@ import 'package:domain/plants/entities/plant_model.dart';
 import 'package:domain/plants/repositories/plant_repository.dart';
 import 'package:equatable/equatable.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:reactive_forms/reactive_forms.dart';
 
 import '../../../common/state_management/supabase_auth_cubit/supabase_auth_cubit_state.dart';
+
+enum AddPlantFormFields { title, description, soilType, plantType, public }
 
 class CreatePlantCubit extends Cubit<CreatePlantState> {
   final PlantsRepository _plantsRepository;
   final String userId;
+  late final FormGroup form;
 
-  CreatePlantCubit(this._plantsRepository, {required this.userId}) : super(CreatePlantState.initial());
+  CreatePlantCubit(this._plantsRepository, {required this.userId}) : super(CreatePlantState.initial()) {
+    form = FormGroup({
+      'title': FormControl<String>(validators: [Validators.required]),
+      'description': FormControl<String?>(),
+      'soilType': FormControl<Iterable<String>>(),
+      'plantType': FormControl<Iterable<String>>(),
+      'public': FormControl<bool>(validators: [Validators.required], value: true),
+    });
+  }
 
   void createPlant(PlantModel plant) {
     emit(state.copyWith(createPlantState: RequestState.loading));
